f0e03beea47ee4a5550dc036ae795b008fad1e86
dan9110.kim <dannykim1018@gmail.com>
2022-02-12 20:57:51 +0900

    first commit
    first commit

---
+++ invest_site/src/main/java/com/dan/invest/invest_site/InvestSiteApplication.java
@@ -0,0 +1,13 @@
+package com.dan.invest.invest_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestSiteApplication.class, args);
+	}
+
+}
---
+++ invest_site/src/test/java/com/dan/invest/invest_site/InvestSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dan.invest.invest_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
